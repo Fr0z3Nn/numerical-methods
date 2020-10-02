0cbfcda229297e339b52200bd37dd1fda8edf79a
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-03 01:00:18 +0300

    add yakobi

--- src/main/java/ru/projects/methods/TASK_03/Yakobi.java
+++ src/main/java/ru/projects/methods/TASK_03/Yakobi.java
@@ -44,20 +44,106 @@ public class Yakobi {
     }
 
     public void findRoots() {
-        printSolution(solution);
-        for (int i = 0; i < matrix.length; i++) {
-            double sum = 0.0;
-            for (int j = 0; j < matrix.length; j++) {
-                sum += solution[j] * matrix[i][j];
-            }
-            solution[i] = sum;
-        }
-        printSolution(solution);
-    }
+        printHead();
+        double epsilon;
+        double delta;
+        int numberOfIteration = 1;
+        double matrixNorm = normMatrix(augmentedMatrix);
+        double vectorNorm = normVector(augmentedMatrix);
 
+        printSolution(solution);
+        System.out.println();
+
+        do {
+            for (int i = 0; i < augmentedMatrix.length; i++) {
+                double sum = 0.0;
+                for (int j = 0; j < augmentedMatrix.length; j++) {
+                    sum += vector[j] * augmentedMatrix[i][j];
+                }
+                sum += augmentedMatrix[i][augmentedMatrix[i].length - 1];
+                solution[i] = sum;
+
+            }
+
+
+            printSolution(solution);
+
+            epsilon = Math.pow(matrixNorm,numberOfIteration)/(1 - matrixNorm) * vectorNorm;
+            System.out.printf("%2.5f %2.8f\n", epsilon,normOfTwoVectors(solution, vector));
+
+            vector = Arrays.copyOf(solution,solution.length);
+
+            numberOfIteration++;
+        }while (epsilon > accuracy);
+
+        System.out.printf("КОЛИЧЕСТВО ИТЕРАЦИЙ: %d", numberOfIteration);
+
+
+
+
+    }
+    private void printHead(){
+        for(int i = 1; i < matrix.length + 1; i++){
+            System.out.printf("%5s ","X" + i );
+        }
+        System.out.printf("%7s %7s","ε", "δ");
+        System.out.println();
+
+    }
     private void printSolution(double[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.printf("X" + (i + 1) + " = %.2f\n", array[i]);
+            System.out.printf("%2.3f ", array[i]);
         }
+
     }
+    public double normOfTwoVectors(double[] vectorNew, double[] vectorOld){
+        double max = Double.MIN_NORMAL;
+        for(int i = 0; i<vectorNew.length; i ++){
+            double tryStayMax = Math.abs(vectorOld[i] - vectorNew[i]);
+            max = Math.max(tryStayMax, max);
+        }
+        return max;
+    }
+
+    public double normMatrix(double[][] array){
+        return Math.min(normMatrix1(array),normMatrix2(array));
+    }
+
+    public double normVector(double[][] array){
+        double max = Double.MIN_NORMAL;
+        for(int i = 0; i<array.length; i ++){
+            double tryStayMax = Math.abs(array[i][array[i].length - 1]);
+            max = Math.max(tryStayMax, max);
+        }
+        return max;
+    }
+
+    private double normMatrix1(double[][] array){
+        double max = Double.MIN_NORMAL;
+        for (int i = 0; i < array.length; i++) {
+            double tryStayMax = 0;
+            for (int j = 0; j < array.length; j++) {
+                tryStayMax += Math.abs(array[i][j]);
+            }
+            if(tryStayMax > max) {
+                max = tryStayMax;
+            }
+        }
+        return max;
+    }
+
+    private double normMatrix2(double[][] array){
+        double max = Double.MIN_NORMAL;
+        for (int i = 0; i < array.length; i++) {
+            double tryStayMax = 0;
+            for (int j = 0; j < array.length; j++) {
+                tryStayMax += Math.abs(array[j][i]);
+            }
+            if(tryStayMax > max) {
+                max = tryStayMax;
+            }
+        }
+        return max;
+    }
+
 }
