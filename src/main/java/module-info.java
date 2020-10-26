a8cd5fdb381de24af2f9ab9c8dc745659f916513
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-26 10:18:50 +0300

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,5 @@ module ru.projects.methods {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
-    requires commons.math3;
     opens ru.projects.methods.TASK_05;
 }
