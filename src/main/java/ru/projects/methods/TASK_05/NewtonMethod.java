00345297d314fd90f454716871eb39710fc49774
Fr0z3Nn <luckyjohn057@gmail.com>
2020-11-04 23:40:25 +0300

    last change

--- src/main/java/ru/projects/methods/TASK_05/NewtonMethod.java
+++ src/main/java/ru/projects/methods/TASK_05/NewtonMethod.java
@@ -19,16 +19,22 @@ public class NewtonMethod extends Method{
     }
 
     private double Сonverges() {
-        if (fN(leftA) * fN(rightB) >= 0) return 0;
-        if(fN(leftA) * fPP(leftA) < 0|| fN(rightB) * fPP(rightB) < 0){
-            return fN(leftA) * fPP(leftA) == 0 ? rightB : leftA;
+        if (fN(leftA) * fN(rightB) >= 0) return -999;
+        if(Math.abs(fN(leftA) * fPP(leftA)) < Math.abs(fN(rightB) * fPP(rightB))){
+            for(double x = leftA; x < rightB; x+=accuracy){
+                if(fN(x) * fPP(x) > 0) return x;
+            }
+        }else {
+            for(double x = rightB; x > leftA; x-=accuracy){
+                if(fN(x) * fPP(x) > 0) return x;
+            }
         }
-        return fN(leftA) * fPP(leftA) > fN(rightB) * fPP(rightB) && fN(leftA) * fPP(leftA) > 0 && fN(rightB) * fPP(rightB) > 0 ? rightB : leftA;
+        return -999;
     }
 
     public String solve() {
         double zeroApproximation = Сonverges();
-        if (zeroApproximation == 0) return "Условие сходимости не выполняется, выберете другой интервал";
+        if (zeroApproximation == -999) return "Условие сходимости не выполняется, выберете другой интервал";
         StringBuilder result = new StringBuilder();
         result.append(" k               x                f(x)                f'(x)              -f(x)/f'(x)\n");
         int k = 0;
