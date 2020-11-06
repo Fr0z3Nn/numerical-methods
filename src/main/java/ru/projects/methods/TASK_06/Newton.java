2b4bc6595d7fb882f4fc3ad802ba765865afe900
Fr0z3Nn <luckyjohn057@gmail.com>
2020-11-06 15:54:41 +0300

    v1

--- src/main/java/ru/projects/methods/TASK_06/Newton.java
+++ src/main/java/ru/projects/methods/TASK_06/Newton.java
@@ -9,28 +9,41 @@ public class Newton extends Method {
     }
 
     public String solve() {
+        if(Y1 < -1.3 || Y2 > 2.3){
+            return "Введите допустимый Y:[-1.3;2.3]";
+        }
+        if(X1 < 0 || X2 > 1000){
+            return "Введите допустимый X:[0;1000]";
+        }
         StringBuilder builder = new StringBuilder();
         int iteration = 0;
-        double zero_X = (X2 + X1) / 2;
-        double zero_Y = (Y2 + Y1) / 2;
+        double zero_X = (X1 + X2) / 2;
+        double zero_Y = (Y1 + Y2) / 2;
+        double next_X;
+        double next_Y;
         double epsilon;
         do {
 
+            next_X = zero_X - (matrix_A1(zero_X, zero_Y) / matrix_J(zero_X, zero_Y));
+            next_Y = zero_Y - (matrix_A2(zero_X, zero_Y) / matrix_J(zero_X, zero_Y));
 
-            double next_X = zero_X - (matrix_A1(zero_X, zero_Y) / matrix_J(zero_X, zero_Y));
-            double next_Y = zero_Y - (matrix_A2(zero_X, zero_Y) / matrix_J(zero_X, zero_Y));
-
-            epsilon = Math.max(Math.abs(next_X - zero_X), Math.abs(next_Y - next_X));
+            epsilon = Math.max(Math.abs(next_X - zero_X), Math.abs(next_Y - zero_Y));
 
             zero_X = next_X;
             zero_Y = next_Y;
 
             iteration++;
+
             builder.append(String.format("|                   |      %2.5f    |      %2.5f    |      %2.5f    |      %2.5f    |                        |                       |                       |                  |\n", zero_X, F1(zero_X, zero_Y), F1poX(zero_X), F1poY(zero_Y)));
             builder.append(String.format("|         %d        | -------------- | -------------- | -------------- | --------------  |      %2.5f    |    %.5f    |      %2.5f    | %.7f|\n", iteration, matrix_A1(zero_X, zero_Y), matrix_A2(zero_X, zero_Y), matrix_J(zero_X, zero_Y), epsilon));
             builder.append(String.format("|                   |      %2.5f    |      %2.5f    |      %2.5f    |      %2.5f   |                        |                      |                       |                  |\n", zero_Y, F2(zero_X, zero_Y), F2poX(), F2poY(zero_Y)));
             builder.append("|-----------------------------------------------------------------------------------------------------------------------------------------|\n");
+
         } while (epsilon > accuracy);
+
+        if(next_X < X1 || next_X > X2 || next_Y < Y1 || next_Y > Y2 || Double.isNaN(next_X) || Double.isNaN(next_Y)){
+            return "Корней в данном квадрате нет или функции сходтся к решению, нележащему в данном квадрате.\n Выберете другой квадрат.";
+        }
 
         builder.append(String.format("X = %.10f\nY = %.10f\nε = %.10f", zero_X,zero_Y,epsilon));
 
--- src/main/java/ru/projects/methods/TASK_06/Util.java
+++ src/main/java/ru/projects/methods/TASK_06/Util.java
@@ -37,4 +37,37 @@ public class Util {
     public static double matrix_A2(double x, double y) {
         return F1poX(x) * F2(x, y) - F2poX() * F1(x, y);
     }
+
+    //SECOND
+
+    public static double FI1(double x, double y) {
+        //return 0.3 - 0.1 * x * x - 0.2 * y * y;
+        //return Math.sqrt(3 + y - y * y);
+        return Math.sqrt(y + 3) - 1;
+
+    }
+
+    public static double FI1poX(double x, double y) {
+        return 1.0;
+    }
+
+    public static double FI1poY(double x, double y) {
+        return 1.0;
+    }
+
+    public static double FI2(double x, double y) {
+        //return 0.7 - 0.2 * x * x + 0.1 * x * y;
+        //return Math.pow(x + 1, 2) - 3;
+        return 3 * x * x + y * y - 3;
+        //return Math.sqrt(y + 3 - 3 * x * x);
+        //return (3 * x * x - 3) / (1 - y);
+    }
+
+    public static double FI2poX() {
+        return 1.0;
+    }
+
+    public static double FI2poY(double x, double y) {
+        return 1.0;
+    }
 }
